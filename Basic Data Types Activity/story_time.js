413c08ca540a901ba85f42887557dc9d19f1fd2a
alinacyi <caialinayi@gmail.com>
2022-08-03 11:53:33 -0400

    new file

---
+++ Basic Data Types Activity/story_time.js
@@ -0,0 +1,30 @@
+// TODO: Create 4-String variables to introduce develop your story.
+let name = "Molly";
+let greeting = "welcome to the world full of stories!";
+let brother = "Max";
+let story = "fairy tales";
+
+// TODO: Create 3-String variables to set the time period of your story or discuss other number elements.
+let year = 5;
+let howOld = 10;
+let part = 4;
+
+
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+let kingdoms = ["the Charming Kingdom","the Northern Kingdom","the Dwarf Kingdom","the Hidden Kingdom"];
+
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+let hasAttacks = true;
+
+// TODO: Print your story to the console.
+console.log("Hi! My name is " + name + " and " + greeting);
+console.log("Now, let me tell you how I ended up here with my brother, " + brother);
+console.log("Growing up, I've always loved listening to " + story + ".");
+console.log(year + " years ago, when I was " + howOld + ", " + "mom gave me and " + brother + " a book.")
+console.log("We loved reading it and read it everyday.")
+console.log("One day, the book sucked us in, and so we started exploring.")
+console.log("We discovered that this world has " + part + " kingdoms.")
+console.log("We became friends with lots of people and started living in it.")
+console.log("I live in " + kingdoms[0] + " and " + brother + " lives in " + kingdoms[1])
+console.log("You may ask if attacks ever happen...")
+console.log(hasAttacks + " but we have enough protections to keep the world safe.")
